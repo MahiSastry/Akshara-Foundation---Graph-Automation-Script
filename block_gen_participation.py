aa9a4ae925b20a22425fe4d4103cb41b16e1eccf
MahiSastry <103404173+MahiSastry@users.noreply.github.com>
2024-12-21 08:13:37 +0530

    Update block_gen_participation.py

--- block_gen_participation.py
+++ block_gen_participation.py
@@ -11,54 +11,44 @@ from kannada_transl import english_to_kannada
 def blockwise_gen_participate(df,grade,dir_name):
     grouped_data_gender = df.groupby(['District', 'Block', 'Gender'])['name_identifier'].nunique().reset_index(name='Count')
 
-    # Specify the folder to save graphs
+    # output folder to save graphs
     output_folder = r'participation_graphs/'
     full_output_path = os.path.join(dir_name, output_folder)
-
-    # Create the output folder if it doesn't exist
+    # create one if it doesn't exist
     os.makedirs(full_output_path, exist_ok=True)
 
-    # Iterate through each district to create a tally table and save the CSV
+    # iterate through the district , create tally tables for count verification
     for district_name, district_data in grouped_data_gender.groupby('District'):
-        
-        # Pivot the data for each district
         tally_table = district_data.pivot_table(index=['Block'], 
                                                 columns=['Gender'], 
                                                 values='Count', 
                                                 aggfunc='sum', 
                                                 fill_value=0).reset_index()
 
-        # Rename columns to indicate Boys and Girls
         tally_table.rename(columns={'male': 'Total Boys', 'female': 'Total Girls'}, inplace=True)
 
-        # Calculate the total population for each block (boys + girls)
         tally_table['Total Population'] = tally_table['Total Boys'] + tally_table['Total Girls']
 
-        # Save the tally table for each district
         tally_filename = os.path.join(full_output_path, f'{grade}_participation_tally_table_{district_name}.csv')
         tally_table.to_csv(tally_filename, index=False)
         print(f"Tally table for {district_name} saved successfully as:", tally_filename)
     custom_palette = {'male': '#AAC698', 'female': '#72A054'}
 
-    # Iterate through each district
+    # graphing section - english
     districts = grouped_data_gender['District'].unique()
     for district in districts:
         # Filter data for the current district
         district_data = grouped_data_gender[grouped_data_gender['District'] == district]
-        
-        # Initialize figure and axis
         fig, ax = plt.subplots()
-        
-       # Plot grouped bar chart using Seaborn
         sns.barplot(data=district_data, x='Block', y='Count', hue='Gender', ax=ax, palette=custom_palette, legend= False)
-
-        # Customize plot
         ax.set_title(f'District: {district.title()}', color='#4D4D4D')
-        
-        # Set axis labels to dark grey
+    
+        # customize axis labels
         ax.set_xlabel('Block', fontsize=12, color='#4E4D4D')
         ax.set_ylabel('Number of Children', fontsize=12, color='#4E4D4D')
 
+        
+        #size of the bars and making sure they don't crowd the graphs - for aesthetic purposes
         for p in ax.patches:
             height = p.get_height()
             print(f'Bar height: {height}')  # Debugging print to check the height of each bar
@@ -70,14 +60,14 @@ def blockwise_gen_participate(df,grade,dir_name):
                             textcoords='offset points')
         #ax.set_xticklabels([label.get_text().title() for label in ax.get_xticklabels()], rotation=45, color='#4D4D4D') 
         
-        # Customize tick labels to dark grey
+        # setting tick labels to dark grey
         ax.tick_params(axis='x', rotation=45, colors='#4E4D4D') 
         ax.tick_params(axis='y', colors='#4E4D4D') 
 
         
         #plt.tight_layout()
 
-        # Change legend text to dark grey
+        # change legend text to dark grey
         '''leg = ax.legend(title='Gender')
         for text in leg.get_texts():
             text.set_color('#4E4D4D')  # Dark grey for legend labels
@@ -119,13 +109,13 @@ def blockwise_gen_participate(df,grade,dir_name):
     fig.update_layout(
         width=1000,
         height=600,
-        xaxis_title='ಬ್ಲಾಕ್',  # Block in Kannada
-        yaxis_title='ವಿದ್ಯಾರ್ಥಿಗಳ ಸಂಖ್ಯೆ',  # Number of students in Kannada
-        title=f'District: {district.title()}',  # Title in Kannada
-        plot_bgcolor='white',   # White background for the plot area
+        xaxis_title='ಬ್ಲಾಕ್',  # block in Kannada
+        yaxis_title='ವಿದ್ಯಾರ್ಥಿಗಳ ಸಂಖ್ಯೆ',  # number of students in Kannada
+        title=f'District: {district.title()}',  
+        plot_bgcolor='white',   
         paper_bgcolor='white',
-        #xaxis=dict(showgrid=True, gridcolor='black'),  # Show gridlines on x-axis
-        #yaxis=dict(showgrid=True, gridcolor='black')   # Show gridlines on y-axis# White background for the overall figure
+        #xaxis=dict(showgrid=True, gridcolor='black'),  
+        #yaxis=dict(showgrid=True, gridcolor='black')   
     )
 
     fig.update_layout(
@@ -141,7 +131,7 @@ def blockwise_gen_participate(df,grade,dir_name):
         )
     )
 
-    # Update x-axis to make text bold
+    #  update x-axis to make text bold
     fig.update_xaxes(
         tickfont=dict(
             family='Arial Bold, sans-serif',
@@ -150,7 +140,7 @@ def blockwise_gen_participate(df,grade,dir_name):
         )
     )
 
-    # Update traces for text display
+    
     fig.update_traces(
         texttemplate='%{text:.}',
         textposition='outside'
